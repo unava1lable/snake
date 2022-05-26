bedf7b1c7aceb6af2f4f1e3e3987b417f14c37bf
unava1lable <910654119@qq.com>
2022-05-26 18:00:44 +0800

    implement game structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use ggez::{ContextBuilder, GameResult, event, conf};
+
+const SCREEN_SIZE: (f32, f32) = (960.0, 680.0);
+
+struct GameState {
+
+}
+
+impl event::EventHandler for GameState {
+    fn update(&mut self, _ctx: &mut ggez::Context) -> GameResult {
+        Ok(())
+    }
+
+    fn draw(&mut self, _ctx: &mut ggez::Context) -> GameResult {
+        Ok(())
+    }
+}
+
+fn main() -> GameResult {
+    let (ctx, event_loop) = ContextBuilder::new("Snake", "Jerry")
+        .window_setup(conf::WindowSetup::default().title("Snake"))
+        .window_mode(conf::WindowMode::default().dimensions(SCREEN_SIZE.0, SCREEN_SIZE.1))
+        .build()?;
+    
+    let game_state = GameState {};
+
+    event::run(ctx, event_loop, game_state)
 }
