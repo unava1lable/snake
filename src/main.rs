5fd2d3b6d7ecff98b29d8d1d28b3889738f126a9
unava1lable <910654119@qq.com>
2022-05-27 11:39:04 +0800

    set background (white)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use ggez::{ContextBuilder, GameResult, event, conf};
+use ggez::{ContextBuilder, GameResult, Context, event, conf, graphics};
 
 const SCREEN_SIZE: (f32, f32) = (960.0, 680.0);
 
@@ -7,16 +7,18 @@ struct GameState {
 }
 
 impl event::EventHandler for GameState {
-    fn update(&mut self, _ctx: &mut ggez::Context) -> GameResult {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
         Ok(())
     }
 
-    fn draw(&mut self, _ctx: &mut ggez::Context) -> GameResult {
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(ctx, [1.0, 1.0, 1.0, 1.0].into());
+        graphics::present(ctx)?;
         Ok(())
     }
 }
 
-fn main() -> GameResult {
+fn main() -> GameResult<()> {
     let (ctx, event_loop) = ContextBuilder::new("Snake", "Jerry")
         .window_setup(conf::WindowSetup::default().title("Snake"))
         .window_mode(conf::WindowMode::default().dimensions(SCREEN_SIZE.0, SCREEN_SIZE.1))
