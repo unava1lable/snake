a7a4d71b49326ac022d4b930dce68a07440db745
unava1lable <910654119@qq.com>
2022-05-27 12:45:16 +0800

    add struct GridPosition

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,40 @@
-use ggez::{ContextBuilder, GameResult, Context, event, conf, graphics};
+use ggez::{Context, ContextBuilder, GameResult};
+use ggez::{conf, event, graphics};
+use oorandom::Rand32;
 
 const SCREEN_SIZE: (f32, f32) = (960.0, 680.0);
 
-struct GameState {
-
+struct GridPosition {
+    x: u16,
+    y: u16,
 }
+
+impl From<(u16, u16)> for GridPosition {
+    fn from(pos: (u16, u16)) -> Self {
+        Self {
+            x: pos.0,
+            y: pos.1
+        }
+    }
+}
+
+impl GridPosition {
+    fn new(x: u16, y: u16) -> Self {
+        Self {
+            x,
+            y,
+        }
+    }
+
+    fn random(rng: &mut Rand32, max_x: u16, max_y: u16) -> Self {
+        (
+            rng.rand_range(0..max_x as u32) as u16,
+            rng.rand_range(0..max_y as u32) as u16
+        ).into()
+    }
+}
+
+struct GameState {}
 
 impl event::EventHandler for GameState {
     fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
